f8b7a8b9a2a0beea28ad13401f5bd404378edcd6
blairt001 <blairt371.dev@gmail.com>
2018-12-26 18:53:38 +0300

    [Feature #162845924](#03)Set up migrations for the database

---
+++ app/migrations/db.py
@@ -0,0 +1,68 @@
+#Making Database Migrations 
+import psycopg2
+import psycopg2.extras
+import os
+from .initial1 import migrations
+from config import BaseConfig
+from ..utils import db_config
+
+
+class Database:
+    def __init__(self):
+        self.config = db_config()
+        self.database = self.config.get('database')
+
+    def migrate(self):
+        con = psycopg2.connect(**self.config)
+        con.autocommit = True
+        cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("select * from pg_database where datname = %(database_name)s", {'database_name': self.database})
+        databases = cur.fetchall()
+        if len(databases) > 0:
+            print(" * Database {} exists".format(self.database))
+            for command in migrations:
+                try:
+                    cur.execute(command)
+                    con.commit()
+                except Exception as e:
+                    print(e)
+        else:
+            print(" * Database {} does not exists".format(self.database))
+        con.close()
+
+    def migrate_test_db(self):
+        """ Create test database and schema """
+        os.environ['APP_SETTINGS'] = 'TESTING'
+        con = psycopg2.connect(**self.config)
+        con.autocommit = True
+        cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        print('\n * Creating test db\n')
+        try:
+            cur.execute('CREATE DATABASE {} OWNER {};'.format(BaseConfig.TEST_DB, self.config.get('user')))
+        except:
+            pass
+        con.close()
+        self.config['database'] = BaseConfig.TEST_DB
+        con = psycopg2.connect(**self.config)
+        cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        for command in migrations:
+            try:
+                cur.execute(command)
+                con.commit()
+                pass
+            except Exception as e:
+                print(e)
+        con.close()
+
+    def drop_test_database(self):
+        print('\n * Dropping test database \n')
+        os.environ['APP_SETTINGS'] = 'PRODUCTION'
+        self.config = db_config()
+        con = psycopg2.connect(**self.config)
+        con.autocommit = True
+        cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute('DROP DATABASE IF EXISTS {};'.format(BaseConfig.TEST_DB, self.config.get('user')))
+        con.close()
+
+
+db = Database()
---
+++ app/migrations/initial1.py
@@ -0,0 +1,114 @@
+"""
+    Commands used for creating and dropping database tables.
+"""
+drop_tables_commands = (
+    """
+    DROP TABLE IF EXISTS users CASCADE
+    """,
+    """
+    DROP TABLE IF EXISTS questions CASCADE
+    """,
+    """
+    DROP TABLE IF EXISTS answers CASCADE
+    """,
+    """
+    DROP TABLE IF EXISTS comments CASCADE
+    """,
+)
+
+drop_answers = (
+    """
+    DROP TABLE IF EXISTS answers CASCADE
+    """,
+)
+
+drop_votes = (
+    """
+    DROP TABLE IF EXISTS votes CASCADE
+    """,
+)
+
+drop_comments = (
+    """
+    DROP TABLE IF EXISTS comments CASCADE
+    """,
+)
+
+create_tables_commands = (
+    """
+    CREATE TABLE IF NOT EXISTS users(
+        user_id SERIAL PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(500) NOT NULL,
+        created_at timestamp with time zone DEFAULT now()
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS questions(
+        question_id SERIAL PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        body text NOT NULL,
+        created_at timestamp with time zone DEFAULT now(),
+        FOREIGN KEY (user_id)
+            REFERENCES users (user_id)
+            ON UPDATE CASCADE ON DELETE CASCADE
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS answers(
+        answer_id SERIAL PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        question_id INTEGER NOT NULL,
+        answer_body TEXT NOT NULL,
+        accepted bool DEFAULT false,
+        created_at timestamp with time zone DEFAULT now(),
+        FOREIGN KEY (question_id)
+            REFERENCES  questions (question_id)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (user_id)
+            REFERENCES users (user_id)
+            ON UPDATE CASCADE ON DELETE CASCADE            
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS comments (
+       comment_id SERIAL PRIMARY KEY,
+       answer_id  INTEGER NOT NULL,
+       user_id INTEGER NOT NULL,
+       comment_body VARCHAR(255) NOT NULL,
+       created_at timestamp with time zone DEFAULT now(),
+       FOREIGN KEY (answer_id)
+           REFERENCES  answers (answer_id)
+           ON UPDATE CASCADE ON DELETE CASCADE,
+       FOREIGN KEY (user_id)
+           REFERENCES users (user_id)
+           ON UPDATE CASCADE ON DELETE CASCADE            
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS votes (
+       vote_id SERIAL PRIMARY KEY,
+       answer_id  INTEGER NOT NULL,
+       user_id INTEGER NOT NULL,
+       vote bool DEFAULT false,
+       created_at timestamp with time zone DEFAULT now(),
+       FOREIGN KEY (answer_id)
+           REFERENCES  answers (answer_id)
+           ON UPDATE CASCADE ON DELETE CASCADE,
+       FOREIGN KEY (user_id)
+           REFERENCES users (user_id)
+           ON UPDATE CASCADE ON DELETE CASCADE            
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS token_blacklist (
+       id SERIAL PRIMARY KEY,
+       token  text NOT NULL,       
+       created_at timestamp with time zone DEFAULT now()           
+    )
+    """,
+)
+
+migrations = create_tables_commands
